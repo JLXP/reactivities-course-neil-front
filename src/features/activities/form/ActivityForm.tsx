c8a6e998041ab29bfb23ed6b5fcb0c9df9dcf765
JavierXool <javier.xool@outlook.com>
2025-08-23 10:51:37 -0600

    Styling the Activity Card

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -37,7 +37,7 @@ export default function ActivityForm() {
   return (
     <Paper sx={{ borderaRadius: 3, padding: 3 }}>
       <Typography variant="h5" gutterBottom color="primary">
-        Create Activity
+        {activity ? "Edit activity" : "Create activity"}
       </Typography>
       <Box
         component="form"
