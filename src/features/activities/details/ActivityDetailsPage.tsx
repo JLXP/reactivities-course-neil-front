7cad32d4c6ab7ae98abee68f68c570f1243de453
JavierXool <javier.xool@outlook.com>
2025-08-23 11:07:42 -0600

    Styling the Activity Details Component

---
+++ src/features/activities/details/ActivityDetailsPage.tsx
@@ -0,0 +1,31 @@
+import {
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useParams } from "react-router";
+import { useActivities } from "../../../lib/hooks/useActivities";
+import ActivityDetailsHeader from "./ActivityDetailsHeader";
+import ActivityDetailsInfo from "./ActivityDetailsInfo";
+import ActivityDetailsChat from "./ActivityDetailsChat";
+import ActivityDetailsSideBar from "./ActivityDetailsSideBar";
+
+export default function ActivityDetails() {
+  const { id } = useParams();
+  const { activity, isLoadingActivity } = useActivities(id);
+
+  if (isLoadingActivity) return <Typography>Loading...</Typography>;
+  if (!activity) return <Typography>Activity not found...</Typography>;
+
+  return (
+    <Grid container spacing={3}>
+      <Grid size={8}>
+        <ActivityDetailsHeader />
+        <ActivityDetailsInfo />
+        <ActivityDetailsChat />
+      </Grid>
+      <Grid size={4}>
+        <ActivityDetailsSideBar />
+      </Grid>
+    </Grid>
+  );
+}
---
+++ src/features/activities/details/ActivityDetailsSideBar.tsx
@@ -0,0 +1,5 @@
+export default function ActivityDetailsSideBar() {
+  return (
+    <div>ActivityDetailsSideBar</div>
+  )
+}
