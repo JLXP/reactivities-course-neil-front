f93b175a034aa3074b6c6822c966738859940017
JavierXool <javier.xool@outlook.com>
2025-08-31 02:32:13 -0600

    Validating the inputs

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,24 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useParams } from "react-router";
-import { useForm, type FieldValues } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useEffect } from "react";
+import {
+  activitySchema,
+  type ActivitySchema,
+} from "../../../lib/schemas/activitySchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function ActivityForm() {
-  const { register, reset, handleSubmit } = useForm();
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ActivitySchema>({
+    mode: "onTouched",
+    resolver: zodResolver(activitySchema),
+  });
   const { id } = useParams();
   const { updateActivity, createActivity, activity, isLoadingActivity } =
     useActivities(id);
@@ -14,7 +27,7 @@ export default function ActivityForm() {
     if (activity) reset(activity);
   }, [activity, reset]);
 
-  const onSubmit = async (data: FieldValues) => {
+  const onSubmit = async (data: ActivitySchema) => {
     console.log(data);
   };
   if (isLoadingActivity) return <Typography>Loading activity...</Typography>;
@@ -35,6 +48,8 @@ export default function ActivityForm() {
           {...register("title")}
           label="Title"
           defaultValue={activity?.title}
+          error={!!errors.title}
+          helperText={errors.title?.message}
         />
         <TextField
           {...register("description")}
@@ -42,27 +57,41 @@ export default function ActivityForm() {
           multiline
           rows={3}
           defaultValue={activity?.description}
+          error={!!errors.description}
+          helperText={errors.description?.message}
         />
         <TextField
           {...register("category")}
           label="Category"
           defaultValue={activity?.category}
+          error={!!errors.category}
+          helperText={errors.category?.message}
         />
         <TextField
           {...register("date")}
           label="Date"
           type="date"
+          error={!!errors.date}
+          helperText={errors.date?.message}
           defaultValue={
             activity?.date
               ? new Date(activity.date).toISOString().split("T")[0]
               : new Date().toISOString().split("T")[0]
           }
         />
-        <TextField {...register("city")} label="City" value={activity?.city} />
+        <TextField
+          {...register("city")}
+          label="City"
+          value={activity?.city}
+          error={!!errors.city}
+          helperText={errors.city?.message}
+        />
         <TextField
           {...register("venue")}
           label="Venue"
           value={activity?.venue}
+          error={!!errors.venue}
+          helperText={errors.venue?.message}
         />
         <Box display="flex" justifyContent="end" gap={3}>
           <Button color="inherit">Cancel</Button>
