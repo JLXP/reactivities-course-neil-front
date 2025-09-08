17b4f961d0dcfa792ede7ba795fd18ce6e05f26b
JavierXool <javier.xool@outlook.com>
2025-09-08 14:47:10 -0600

    Handling API validation errors

--- src/features/account/RegisterForm.tsx
+++ src/features/account/RegisterForm.tsx
@@ -4,14 +4,18 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Button, Paper, Typography } from "@mui/material";
 import { LockOpen } from "@mui/icons-material";
 import TextInput from "../../app/shared/components/TextInput";
-import { Link,} from "react-router";
-import { registerSchema, type RegisterShema } from '../../lib/schemas/registerSchema';
+import { Link } from "react-router";
+import {
+  registerSchema,
+  type RegisterShema,
+} from "../../lib/schemas/registerSchema";
 
 export default function RegisterForm() {
   const { registerUser } = useAccount();
   const {
     control,
     handleSubmit,
+    setError,
     formState: { isValid, isSubmitting },
   } = useForm<RegisterShema>({
     mode: "onTouched",
@@ -19,7 +23,16 @@ export default function RegisterForm() {
   });
 
   const onSubmit = async (data: RegisterShema) => {
-    await registerUser.mutateAsync(data);
+    await registerUser.mutateAsync(data, {
+      onError: (error) => {
+        if (Array.isArray(error)) {
+          error.forEach((err) => {
+            if (err.includes("Email")) setError("email", { message: err });
+            else if (err.includes("Password")) setError("password", { message: err });
+          });
+        }
+      },
+    });
   };
 
   return (
@@ -47,8 +60,17 @@ export default function RegisterForm() {
         <Typography variant="h4">Register</Typography>
       </Box>
       <TextInput label="Email" control={control} name="email"></TextInput>
-      <TextInput label="Display name" control={control} name="displayName"></TextInput>
-      <TextInput label="Password" type="password" control={control} name="password"></TextInput>
+      <TextInput
+        label="Display name"
+        control={control}
+        name="displayName"
+      ></TextInput>
+      <TextInput
+        label="Password"
+        type="password"
+        control={control}
+        name="password"
+      ></TextInput>
       <Button
         type="submit"
         disabled={!isValid || isSubmitting}
@@ -59,12 +81,7 @@ export default function RegisterForm() {
       </Button>
       <Typography sx={{ textAlign: "center" }}>
         Already have an account?
-        <Typography
-          sx={{ ml: 2 }}
-          component={Link}
-          to="/login"
-          color="primary"
-        >
+        <Typography sx={{ ml: 2 }} component={Link} to="/login" color="primary">
           Sign in
         </Typography>
       </Typography>
--- src/lib/hooks/useAccount.ts
+++ src/lib/hooks/useAccount.ts
@@ -2,13 +2,14 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { LoginSchema } from "../schemas/loginSchema";
 import agent from "../api/agent";
 import type { User } from "../types";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import type { RegisterShema } from "../schemas/registerSchema";
 import { toast } from "react-toastify";
 
 export const useAccount = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const loginUser = useMutation({
     mutationFn: async (creds: LoginSchema) => {
@@ -48,7 +49,10 @@ export const useAccount = () => {
       const response = await agent.get<User>("/account/user-info");
       return response.data;
     },
-    enabled: !queryClient.getQueryData(["user"]),
+    enabled:
+      !queryClient.getQueryData(["user"]) &&
+      location.pathname !== "login" &&
+      location.pathname !== "/register",
   });
 
   return {
@@ -56,6 +60,6 @@ export const useAccount = () => {
     currentUser,
     logoutUser,
     loadingUserInfo,
-    registerUser
+    registerUser,
   };
 };
