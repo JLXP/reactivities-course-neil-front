769e4f53e7973f750a96f5f4c14024d8176432f3
JavierXool <javier.xool@outlook.com>
2025-10-01 23:21:12 -0600

    Create editProfileSchema

---
+++ src/lib/schemas/editProfileSchema.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+import { requiredString } from "../utils/utils";
+
+export const editProfileSchema = z.object({
+  displayName: requiredString("Display Name"),
+  bio: z.string().optional,
+});
+
+
+export type EditProfileSchema = z.infer<typeof editProfileSchema>;
