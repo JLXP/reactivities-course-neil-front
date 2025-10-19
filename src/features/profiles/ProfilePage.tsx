e929a7f6fe4ad3cc92c36ce9f46a6733d468b62c
JavierXool <javier.xool@outlook.com>
2025-10-19 16:27:46 -0600

    Adding a followers component

--- src/features/profiles/ProfilePage.tsx
+++ src/features/profiles/ProfilePage.tsx
@@ -17,7 +17,7 @@ export default function ProfilePage() {
   return (
     <Grid>
         <Grid size={2}>
-            <ProfileHeader profile={profile}/>
+            <ProfileHeader/>
             <ProfileContent/>
         </Grid>
     </Grid>
--- src/lib/hooks/useProfile.ts
+++ src/lib/hooks/useProfile.ts
@@ -4,7 +4,7 @@ import type { Photo, Profile, User } from "../types";
 import { useMemo } from "react";
 import type { EditProfileSchema } from "../schemas/editProfileSchema";
 
-export const useProfile = (id?: string) => {
+export const useProfile = (id?: string, predicate?: string) => {
   const queryClient = useQueryClient();
 
   const { data: profile, isLoading: loadingProfile } = useQuery<Profile>({
@@ -13,7 +13,7 @@ export const useProfile = (id?: string) => {
       const response = await agent.get<Profile>(`/profiles/${id}`);
       return response.data;
     },
-    enabled: !!id,
+    enabled: !!id && !!predicate,
   });
 
   const { data: photos, isLoading: loadingPhotos } = useQuery<Photo[]>({
@@ -22,10 +22,21 @@ export const useProfile = (id?: string) => {
       const response = await agent.get<Photo[]>(`/profiles/${id}/photos`);
       return response.data;
     },
-    enabled: !!id,
+    enabled: !!id && !!predicate,
   });
 
-  
+  const { data: followings, isLoading: loadingFollowings } = useQuery<
+    Profile[]
+  >({
+    queryKey: ["followings", id, predicate],
+    queryFn: async () => {
+      const response = await agent.get<Profile[]>(
+        `/profiles/${id}/follow-list?predicate=${predicate}`
+      );
+      return response.data;
+    },
+    enabled: !!id && !!predicate
+  });
 
   const uploadPhoto = useMutation({
     mutationFn: async (file: Blob) => {
@@ -115,28 +126,27 @@ export const useProfile = (id?: string) => {
   });
 
   const updateFollowing = useMutation({
-    mutationFn:async ()=>{
-      await agent.post(`/profiles/${id}/follow`)
+    mutationFn: async () => {
+      await agent.post(`/profiles/${id}/follow`);
     },
-    onSuccess:()=>{
-      queryClient.setQueryData(["profile", id],(profile:Profile)=>{
-        if(!profile || profile.followerCount === undefined) return profile;
+    onSuccess: () => {
+      queryClient.setQueryData(["profile", id], (profile: Profile) => {
+        queryClient.invalidateQueries({queryKey:['followings', id, 'followers']})
+        if (!profile || profile.followerCount === undefined) return profile;
         return {
           ...profile,
           following: !profile.following,
-          followersCount: profile.following 
-          ? profile.followerCount -1
-          : profile.followerCount +1
-        }
-      })
-    }
-  })
+          followersCount: profile.following
+            ? profile.followerCount - 1
+            : profile.followerCount + 1,
+        };
+      });
+    },
+  });
 
   const isCurrentUser = useMemo(() => {
     return id === queryClient.getQueryData<User>(["user"])?.id;
   }, [id, queryClient]);
-
-  
 
   return {
     profile,
@@ -148,6 +158,8 @@ export const useProfile = (id?: string) => {
     setMainPhoto,
     deletePhoto,
     updateProfile,
-    updateFollowing
+    updateFollowing,
+    followings,
+    loadingFollowings
   };
 };
